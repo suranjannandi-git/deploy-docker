84024af6b3028322a8411c08fe57c68392bf17ad
surnandi <suranjan_nandi@yahoo.com>
2025-03-16 17:45:21 +0530

    Update home.py

--- src/home.py
+++ src/home.py
@@ -3,7 +3,7 @@ import streamlit as st
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
-env_value = os.getenv("WX_CLOUD_URL", None)
+env_value = os.getenv("mykey", None)
 st.write(f"env_value: {env_value}")
 
 text = st.chat_input("Type a message...")
